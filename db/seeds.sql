a95825d372c6eaa8aeaa19e27fe72b7c0ea59f54
winniemlau <wmlau26@gmail.com>
2016-05-12 12:07:44 -0400

    inital

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO myBurgers (burger_name, devour) VALUES (
+	"BBQ Chicken Burger",
+	0
+);
+INSERT INTO myBurgers (burger_name, devour) VALUES (
+	"Cheeseburger",
+	0
+);
+INSERT INTO myBurgers (burger_name, devour) VALUES (
+	"California Burger",
+	1
+);
